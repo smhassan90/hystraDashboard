41050fe164e7346ba8d6198f9ec44c38a9987c39
areeb <areebahmedkhan91@gmail.com>
2023-05-02 16:10:32 +0500

    report component

--- HYSTRA Dashboard/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ HYSTRA Dashboard/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,10 +5,12 @@ import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
+import { ReportsComponent } from 'src/app/pages/reports/reports.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'reports',        component: ReportsComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent }
