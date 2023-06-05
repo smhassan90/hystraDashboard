009cff6bef00352c542eca8f5c585019777b3d18
Tehnan <tehnan20@gmail.com>
2023-06-05 16:25:44 +0500

    Filters added to dashboard

--- HYSTRA Dashboard/src/app/components/navbar/navbar.component.ts
+++ HYSTRA Dashboard/src/app/components/navbar/navbar.component.ts
@@ -9,6 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  public KarachiSelected: boolean = true;
+  public RawalpindiSelected: boolean = false;
+  public AllDistrictsSelected: boolean = false;
+
+  public MTDSelected: boolean = true;
+  public YTDSelected: boolean = false;
+  public PTDSelected: boolean = false;
+
+  public SelectedCity: string = "Karachi";
+  public SelectedPeriodFilter: string = "MTD";
+
   public focus;
   public listTitles: any[];
   public location: Location;
@@ -31,6 +43,54 @@ export class NavbarComponent implements OnInit {
         }
     }
     return 'Dashboard';
+  }
+
+  public SelectKarachi(): void
+  {
+    this.SelectedCity = "Karachi";
+    this.KarachiSelected = true;
+    this.RawalpindiSelected = false;
+    this.AllDistrictsSelected = false;
+  }
+
+  public SelectRawalpindi(): void
+  {
+    this.SelectedCity = "Rawalpindi";
+    this.RawalpindiSelected = true;
+    this.KarachiSelected = false;
+    this.AllDistrictsSelected = false;
+  }
+
+  public SelectAllDistricts(): void
+  {
+    this.SelectedCity = "All Districts";
+    this.KarachiSelected = true;
+    this.RawalpindiSelected = true;
+    this.AllDistrictsSelected = true;
+  }
+
+  public SelectMTD(): void
+  {
+    this.SelectedPeriodFilter = "MTD";
+    this.MTDSelected = true;
+    this.YTDSelected = false;
+    this.PTDSelected = false;
+  }
+
+  public SelectYTD(): void
+  {
+    this.SelectedPeriodFilter = "YTD";
+    this.YTDSelected = true;
+    this.PTDSelected = false;
+    this.MTDSelected = false;
+  }
+
+  public SelectPTD(): void
+  {
+    this.SelectedPeriodFilter = "PTD";
+    this.PTDSelected = true;
+    this.MTDSelected = false;
+    this.YTDSelected = false;
   }
 
 }
