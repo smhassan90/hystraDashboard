6da1dbe4caa90500442e6c01e39fd8832065c4d5
Tehnan <tehnan20@gmail.com>
2023-09-08 17:30:17 +0500

    Date selection added in "Call Executed" section.

--- HYSTRA Dashboard/src/app/pages/reports/reports.component.ts
+++ HYSTRA Dashboard/src/app/pages/reports/reports.component.ts
@@ -9,39 +9,6 @@ import { NgxPaginationModule } from "ngx-pagination";
 })
 export class ReportsComponent implements OnInit {
 
-  p: Number = 1;
-  count: Number = 10;
-
-  public IsMioReports: boolean = false;
-  public IsCallExecuted: boolean = false;
-  public IsMSS: boolean = false;
-
-  public KarachiSelected: boolean = true;
-  public RawalpindiSelected: boolean = false;
-  public AllDistrictsSelected: boolean = false;
-
-  public MIOSelected: boolean = true;
-  public CHOSelected: boolean = false;
-  public AllTypesSelected: boolean = false;
-
-  public SelectedCity: string = "Karachi";
-  public SelectedType: string = "MIO";
-
-  public MIOBorder: string;
-  public CallsBorder: string;
-  public MMSBorder: string;
-
-  public BusinessAPIData : any = [];
-  public CallsExecutedData : any = [];
-
-  public CallsPlanned : any = [];
-  public Planned : any = [];
-  public CallsUnplanned : any = [];
-  public ActuallyVisited : any = [];
-  public NotVisited : any = [];
-  public PlannedPercentage : any = [];
-  public UnplannedPercentage : any = [];
-
   public MIOReportsRawalpindi = [
     {
       date: "05/02/2023",
@@ -125,43 +92,51 @@ export class ReportsComponent implements OnInit {
     },
   ];
 
+  p: Number = 1;
+  count: Number = 10;
+
+  public IsMioReports: boolean = false;
+  public IsCallExecuted: boolean = false;
+  public IsMSS: boolean = false;
+
+  public KarachiSelected: boolean = true;
+  public RawalpindiSelected: boolean = false;
+  public AllDistrictsSelected: boolean = false;
+
+  public MIOSelected: boolean = true;
+  public CHOSelected: boolean = false;
+  public AllTypesSelected: boolean = false;
+
+  public SelectedCity: string = "Karachi";
+  public SelectedType: string = "MIO";
+
+  public MIOBorder: string;
+  public CallsBorder: string;
+  public MMSBorder: string;
+
+  public BusinessAPIData : any = [];
+  public CallsExecutedData : any = [];
+
+  public CallsPlanned : any = [];
+  public Planned : any = [];
+  public CallsUnplanned : any = [];
+  public ActuallyVisited : any = [];
+  public NotVisited : any = [];
+  public PlannedPercentage : any = [];
+  public UnplannedPercentage : any = [];
+
+  public year: any = "2022";
+  public monthId: any = "04";
+  public monthName: any = "";
+
+
   constructor(private businessService: BusinessService) { }
 
   ngOnInit(): void
   {
-    var year: any = "2022";
-    var monthId: any = "02";
-    var monthName: any = "";
+    // monthName = this.GetMonthNameFromMonthId(monthName, monthId) + " " + year;
 
-    monthName = this.GetMonthNameFromMonthId(monthName, monthId) + " " + year;
-
-    this.businessService.GetBusinessData(year, monthId).subscribe((result) => {
-      this.BusinessAPIData = result;
-      console.log("Business API: ", this.BusinessAPIData);
-
-      for(let i = 0; i < this.BusinessAPIData.length; i++)
-      {
-        this.CallsPlanned[i] = this.BusinessAPIData[i].doctors / this.BusinessAPIData[i].pdoctors;
-        this.Planned[i] = this.BusinessAPIData[i].calls / this.BusinessAPIData[i].pcalls;
-        this.ActuallyVisited[i] = this.BusinessAPIData[i].calls;
-        this.CallsUnplanned[i] = this.ActuallyVisited[i] - this.Planned[i];
-        this.NotVisited[i] = this.BusinessAPIData[i].doctors - this.BusinessAPIData[i].covered;
-        this.PlannedPercentage[i] = (this.BusinessAPIData[i].pcalls / this.BusinessAPIData[i].calls) * 100;
-        this.UnplannedPercentage[i] = 100 - this.Planned[i];
-
-        this.CallsExecutedData.push({
-          Month: monthName,
-          CallsPlanned: this.CallsPlanned[i],
-          Planned: this.Planned[i],
-          CallsUnplanned: this.CallsUnplanned[i],
-          ActuallyVisited: this.ActuallyVisited[i],
-          NotVisited: this.NotVisited[i],
-          PlannedPercentage: this.PlannedPercentage[i],
-          UnplannedPercentage: this.UnplannedPercentage[i]
-        });
-      }
-      console.log("CallsExecutedData: ", this.CallsExecutedData);
-    });
+    this.GetBusinessAPIData(this.year, this.monthId, this.monthName);
 
     this.IsMioReports = true;
     this.MIOBorder = "6px solid #f3603994";
@@ -247,58 +222,103 @@ export class ReportsComponent implements OnInit {
     this.CHOSelected = true;
   }
 
-  public GetMonthNameFromMonthId(monthName, monthId): any
+  public SelectYear(value: any): void
   {
-    if(monthId == "01")
-    {
-      monthName = "Jan"
-    }
-    else if(monthId == "02")
-    {
-      monthName = "Feb"
-    }
-    else if(monthId == "03")
-    {
-      monthName = "Mar"
-    }
-    else if(monthId == "04")
-    {
-      monthName = "Apr"
-    }
-    else if(monthId == "05")
-    {
-      monthName = "May"
-    }
-    else if(monthId == "06")
-    {
-      monthName = "June"
-    }
-    else if(monthId == "07")
-    {
-      monthName = "July"
-    }
-    else if(monthId == "08")
-    {
-      monthName = "Aug"
-    }
-    else if(monthId == "09")
-    {
-      monthName = "Sep"
-    }
-    else if(monthId == "10")
-    {
-      monthName = "Oct"
-    }
-    else if(monthId == "11")
-    {
-      monthName = "Nov"
-    }
-    else if(monthId == "12")
-    {
-      monthName = "Dec"
-    }
+    this.year = value;
+  }
 
-    return monthName;
+  public SelectMonth(value: any): void
+  {
+    this.monthName = value;
+
+    if(this.monthName == "January")
+    {
+      this.monthId = "01"
+    }
+    else if(this.monthName == "February")
+    {
+      this.monthId = "02"
+    }
+    else if(this.monthName == "March")
+    {
+      this.monthId = "03"
+    }
+    else if(this.monthName == "April")
+    {
+      this.monthId = "04"
+    }
+    else if(this.monthName == "May")
+    {
+      this.monthId = "05"
+    }
+    else if(this.monthName == "June")
+    {
+      this.monthId = "06"
+    }
+    else if(this.monthName == "July")
+    {
+      this.monthId = "07"
+    }
+    else if(this.monthName == "August")
+    {
+      this.monthId = "08"
+    }
+    else if(this.monthName == "September")
+    {
+      this.monthId = "09"
+    }
+    else if(this.monthName == "October")
+    {
+      this.monthId = "10"
+    }
+    else if(this.monthName == "November")
+    {
+      this.monthId = "11"
+    }
+    else if(this.monthName == "December")
+    {
+      this.monthId = "12"
+    }
+    this.monthName = value + " " + this.year;
+    console.log(this.monthName);
+
+    for(let i = 0; i < this.BusinessAPIData.length; i++)
+    {
+      this.BusinessAPIData[i].year = this.year;
+      this.BusinessAPIData[i].month = this.monthId;
+      this.CallsExecutedData[i].Month = this.monthName;
+    }
+  }
+
+  public GetBusinessAPIData(year: any, monthid: any, monthname: any): void
+  {
+    this.businessService.GetBusinessData(year, monthid).subscribe((result) => {
+      this.BusinessAPIData = result;
+      console.log("Business API: ", result);
+
+      for(let i = 0; i < this.BusinessAPIData.length; i++)
+      {
+        this.CallsPlanned[i] = this.BusinessAPIData[i].doctors / this.BusinessAPIData[i].pdoctors;
+        this.Planned[i] = this.BusinessAPIData[i].calls / this.BusinessAPIData[i].pcalls;
+        this.ActuallyVisited[i] = this.BusinessAPIData[i].calls;
+        this.CallsUnplanned[i] = this.ActuallyVisited[i] - this.Planned[i];
+        this.NotVisited[i] = this.BusinessAPIData[i].doctors - this.BusinessAPIData[i].covered;
+        this.PlannedPercentage[i] = (this.BusinessAPIData[i].pcalls / this.BusinessAPIData[i].calls) * 100;
+        this.UnplannedPercentage[i] = 100 - this.Planned[i];
+
+        this.CallsExecutedData.push({
+          Month: monthname,
+          CallsPlanned: this.CallsPlanned[i],
+          Planned: this.Planned[i],
+          CallsUnplanned: this.CallsUnplanned[i],
+          ActuallyVisited: this.ActuallyVisited[i],
+          NotVisited: this.NotVisited[i],
+          PlannedPercentage: this.PlannedPercentage[i],
+          UnplannedPercentage: this.UnplannedPercentage[i]
+        });
+      }
+      console.log("CallsExecutedData: ", this.CallsExecutedData);
+    });
   }
 
 }
